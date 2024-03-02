67c11ef174dc986d2e44c06861ae2d5b076c7db8
darkars33 <darshangarad18@gmail.com>
2024-03-02 23:11:12 +0530

    create line component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import homeScreen from "../assets/homeScreen.svg";
+import Line from "../Components/Line";
 
 const Home = () => {
-          const backgroundImage= {
-                    backgroundImage: `url(${homeScreen})`,
-                    backgroundRepeat: "none",
-                    backgroundSize: "cover",backgroundPosition: "center",
-                    innerHeight: "full",
-                    innerWidth: "full",
-          }
-          
+  const backgroundImage = {
+    backgroundImage: `url(${homeScreen})`,
+    backgroundRepeat: "none",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    innerHeight: "full",
+    innerWidth: "full",
+  };
+
   return (
     <>
       <div
-        className="w-full h-[100vh] flex justify-center align-middle items-center"
+        className="w-full h-[90vh] flex justify-center items-end"
         style={backgroundImage}
       >
-          <h1 data-text="Nature Club" className="text-white text-[150px] absolute bottom-[160px] ms:text-[100px] ms:bottom-[150px] xs:text-[50px] xs:bottom-[180px]">Nature Club</h1>
+        <h1
+          data-text="Nature Club"
+          className="relative text-white text-[140px] bottom-[123px] select-none ms:text-[100px] xs:text-[40px] xs:bottom-[160px] rounded-md uppercase"
+        >
+          Nature Club
+        </h1>
+      </div>
+      <div className="w-full flex justify-center">
+        <Line />
+        <div className="flex flex-col items-center ">
+        <h1><span className="text-green-600 text-[20px]">5,000+</span></h1>
+        <h2 className="font-medium">No. Of Visitors</h2>
+        </div>
+        <Line />
       </div>
     </>
   );
