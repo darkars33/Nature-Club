df1ab4e61756878282d2636d6996813cc5603f76
darkars33 <darshangarad18@gmail.com>
2024-03-03 15:48:22 +0530

    update in home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import homeScreen from "../assets/homeScreen.svg";
 import Line from "../Components/Line";
+import plant from "../assets/plant.svg";
 
 const Home = () => {
   const backgroundImage = {
@@ -20,18 +21,35 @@ const Home = () => {
       >
         <h1
           data-text="Nature Club"
-          className="relative text-white text-[140px] bottom-[123px] select-none ms:text-[100px] xs:text-[40px] xs:bottom-[160px] rounded-md uppercase"
+          className="relative text-white text-[140px] bottom-[123px] select-none ms:text-[100px] lg:text-[120px] xs:text-[40px] xs:bottom-[160px] rounded-md uppercase "
         >
           Nature Club
         </h1>
       </div>
-      <div className="w-full flex justify-center">
-        <Line />
+      <br />
+      <div className="w-full flex justify-center ">
+        {/* <Line /> */}
         <div className="flex flex-col items-center ">
-        <h1><span className="text-green-600 text-[20px]">5,000+</span></h1>
-        <h2 className="font-medium">No. Of Visitors</h2>
+          <h1>
+            <span className="text-green-600 text-[20px]">5,000+</span>
+          </h1>
+          <h2 className="font-medium">No. Of Visitors</h2>
         </div>
-        <Line />
+        {/* <Line /> */}
+      </div>
+      <br />
+      <div className="w-full h-[40vh]  flex justify-center items-center">
+        <div className="w-[80%] h-[80%] flex justify-between items-center ms:flex-col ms:gap-4">
+          <div className="w-[450px] h-[75%] box flex justify-center flex-col gap-1 ms:w-full lg:h-[100%]">
+              <h1 className="text-[35px] text-center">Our Mission</h1>
+              <p className="text-center leading-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit totam debitis voluptatum quia labore, dignissimos officiis possimus nulla reprehenderit quibusdam magni nisi accusantium eligendi dolorum neque nam illum illo!</p>
+          </div>
+          <img src={plant} alt="" />
+          <div className="w-[450px] h-[75%] box flex justify-center items-center flex-col gap-1  ms:w-full lg:h-[100%]">
+              <h1 className="text-[35px]">Our Vision</h1>
+              <p className=" relative text-center leading-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima velit totam debitis voluptatum quia labore, dignissimos officiis possimus nulla reprehenderit quibusdam magni nisi accusantium eligendi dolorum neque nam illum illo!</p>
+          </div>
+        </div>
       </div>
     </>
   );
