a18ad4ebc5cd4fbc70789fce888a485f8afcfb2c
darkars33 <darshangarad18@gmail.com>
2024-07-21 13:13:03 +0530

    login

--- src/BackendInputs/LogIn.jsx
+++ src/BackendInputs/LogIn.jsx
@@ -2,26 +2,37 @@ import React from "react";
 
 const LogIn = () => {
   return (
-    <div className="w-full h-screen">
-          <div className="w-full h-[10vh] ms:h-[9vh]"></div>
-      <label className="input input-bordered flex items-center gap-2">
-        Name
-        <input type="text" className="grow" placeholder="Daisy" />
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        Email
-        <input type="text" className="grow" placeholder="daisy@site.com" />
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        <input type="text" className="grow" placeholder="Search" />
-        <kbd className="kbd kbd-sm">⌘</kbd>
-        <kbd className="kbd kbd-sm">K</kbd>
-      </label>
-      <label className="input input-bordered flex items-center gap-2">
-        <input type="text" className="grow" placeholder="Search" />
-        <span className="badge badge-info">Optional</span>
-      </label>
-    </div>
+    <>
+      <div className="max-w-screen-xl mx-auto flex h-screen">
+        <div className="flex-1 flex flex-col justify-center items-center">
+          <form className="flex gap-4 flex-col">
+            <h1 className="text-4xl font-extrabold text-black">
+              {"Let's"} go.
+            </h1>
+            <label className="input input-bordered rounded flex items-center gap-2">
+              <input
+                type="text"
+                className="grow"
+                placeholder="username"
+                name="username"
+              />
+            </label>
+            <label className="input input-bordered rounded flex items-center gap-2">
+              <input
+                type="password"
+                className="grow"
+                placeholder="Password"
+                name="password"
+              />
+            </label>
+            <button className="btn rounded-full btn-primary text-white">
+              Sumit
+            </button>
+            {/* {isError && <p className="text-red-500">Something went wrong</p>} */}
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
